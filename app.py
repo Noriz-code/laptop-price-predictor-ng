ec61353ce523959b7af28ba3f326a38757f6ef0f
Chikanma_Kanu <nourisekanu@yahoo.co.uk>
2026-07-14 15:58:17 +0100

    Refactor app.py for laptop price prediction

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ st.set_page_config(page_title="Laptop Price Predictor NG", layout="wide")
 @st.cache_resource
 def load_model():
     return joblib.load(r'C:\Users\userone\Desktop\models\best_laptop_price_model.pkl')
-
-   model = joblib.load('best_laptop_price_model.pkl')
+    model = joblib.load('best_laptop_price_model.pkl')
 
 st.title("💻 Laptop Price Predictor - Nigeria NG")
 st.write("Predict laptop price in NGN using GradientBoost_200")
